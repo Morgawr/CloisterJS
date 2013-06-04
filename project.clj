afa2abb6f1dc91e3ce313d0e3f1657a1b301e0b4
Morgawr <morgawr@gmail.com>
2013-06-05 00:00:35 +0200

    added experimental macros for systems

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "ClojureScript videogame library"
   :url ""
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring "1.1.8"]]
+                 [ring "1.1.8"]
+                 [org.clojure/math.combinatorics "0.0.4"]]
   :plugins [[lein-cljsbuild "0.3.2"]
             [lein-ring "0.8.3"]]
   :hooks [leiningen.cljsbuild]
--- src/cljs/cloisterjs/macros.clj
+++ src/cljs/cloisterjs/macros.clj
@@ -21,3 +21,75 @@
     )
   )
 )
+
+(defmacro get-containers
+  "Given a set of keys and the whole collection of components, returns a 
+  cartesian product of all possible combinations of the given keys taken from
+  the given collection"
+  [names all]
+  `(apply clojure.math.combinatorics/cartesian-product 
+         (map #(% (select-keys ~all ~names)) ~names))
+)
+
+(defmacro filter-components
+  "Given the pairs of components, filter them following a proper ruleset."
+  [rules comps]
+  `(filter (fn [c] (every? #(apply % c) ~rules)) ~comps)
+)
+
+(defmacro remap-containers
+  "Given the list of containers, remap it with the new containers, removing 
+  deleted components"
+  [map1 map2]
+  `(apply dissoc (merge ~map1 ~map2) (map first (remove second ~map2)))
+)
+
+(defmacro reflow-state
+  "Merge all the different temporary containers and put them back into the
+  state"
+  [state in]
+  `(if (empty? ~in)
+     ~state
+     (reduce (partial merge-with remap-containers ~state) ~in)
+  )
+)
+
+(defmacro defsystem
+  "Define a new system, this is a wrapping around defrecord and an automated 
+  call to its constructor. It creates a new system specification with the
+  proper name and parameters which consist of components (input interface), 
+  set of rules for validating said components and a body to manipulate each 
+  interaction."
+  [name components ruleset handler]
+  (let [tname (gensym (str name))
+        nname (str name)
+        tctor (symbol (str (clojure.core/name tname) "."))]
+  `(do
+     (defrecord ~tname [actors rules handler run])
+     (defn ~name []
+       (~tctor 
+         ~components 
+         ~ruleset
+         ~handler
+         (fn [state r]
+           (->> state
+                :containers ; extract containers
+                (get-containers ~components) ; retrieve only the ones we need
+                (filter-components r) ; only those who meet the validation
+                (#(doall (map ~handler %))) ; need doall to resolve lazyness
+                (reflow-state state)
+           )
+        )
+      )
+    )
+  )
+)
+
+
+
+
+
+             
+
+
+

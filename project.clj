5a8c709a7b9e5a4e40ac85c9f7178f18c33309f7
Morgawr <morgawr@gmail.com>
2013-06-14 23:39:21 +0200

    fixed project.clj and added modified zlib license

--- project.clj
+++ project.clj
@@ -2,8 +2,7 @@
   :description "ClojureScript videogame library"
   :url ""
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring "1.1.8"]
-                 [org.clojure/math.combinatorics "0.0.4"]]
+                 [ring "1.1.8"]]
   :plugins [[lein-cljsbuild "0.3.2"]
             [lein-ring "0.8.3"]]
   :hooks [leiningen.cljsbuild]
